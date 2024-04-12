65c62e9d293e7ea2c66c09e0d5fb6a74e48e2913
jmzcayetano <jmzcayetano@gmail.com>
2024-04-12 15:58:38 -0500

    correccion

--- Backend/src/main/java/com/sistema/prestamos/SistemaPrestamosBackendApplication.java
+++ Backend/src/main/java/com/sistema/prestamos/SistemaPrestamosBackendApplication.java
@@ -6,13 +6,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
-public class SistemaExamenesBackendApplication implements CommandLineRunner {
+public class SistemaPrestamosBackendApplication implements CommandLineRunner {
 
 	@Autowired
 	private UsuarioService usuarioService;
 
 	public static void main(String[] args) {
-		SpringApplication.run(SistemaExamenesBackendApplication.class, args);
+		SpringApplication.run(SistemaPrestamosBackendApplication.class, args);
 	}
 
 	@Override
