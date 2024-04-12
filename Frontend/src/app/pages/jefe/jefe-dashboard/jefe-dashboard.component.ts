3696bb26673e25ee919a2fd413c37399602f13bf
jmzcayetano <jmzcayetano@gmail.com>
2024-04-12 15:40:02 -0500

    se desplega correccion

---
+++ Frontend/src/app/pages/jefe/jefe-dashboard/jefe-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jefe-dashboard',
+  templateUrl: './jefe-dashboard.component.html',
+  styleUrls: ['./jefe-dashboard.component.css']
+})
+export class JefeDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
